94bfe9d4259c44a475514d5fc97261ce8f2a5e4a
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-23 20:41:42 -0300

    feat: add description to products

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -11,13 +11,13 @@ const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className="product-card">
       <h2>{name}</h2>
-      <div className="price-div">
-        <h3>Preço:</h3>
-        <p>{`${price.toString()}0`} R$</p>
-      </div>
       <div className="details-div">
         <h3>Detalhes:</h3>
         <p>{description}</p>
+      </div>
+      <div className="price-div">
+        <h3>Preço:</h3>
+        <p>{`${price.toString()}0`} R$</p>
       </div>
       <img src={`${picture}`} alt={`picture ${name}`} />
     </div>
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -3,114 +3,133 @@ export const products = [
     name: "Açafrão",
     price: 5.5,
     picture: "/assets/products/acafrao.png",
-    description: "",
+    description:
+      "O açafrão é extraído dos estigmas da flor Crocus sativus e possui propriedades antioxidantes e anti-inflamatórias, sendo utilizado em diversos pratos como arroz, risotos, sopas e até mesmo em doces. O texto também convida os clientes a comprarem açafrão de alta qualidade em sua loja.",
   },
   {
     name: "Alho Granulado",
     price: 5.5,
     picture: "/assets/products/alho-granulado.png",
-    description: "",
+    description:
+      "O alho granulado é um tempero muito utilizado na culinária, com sabor e aroma intensos. É obtido a partir da moagem do alho fresco e pode ser utilizado em diversas preparações, como molhos, assados, refogados, massas e saladas. Além disso, o alho possui propriedades medicinais, sendo considerado um poderoso antioxidante e anti-inflamatório.",
   },
   {
     name: "Bicarbonato de Sódio",
     price: 4.5,
     picture: "/assets/products/bicarbonato-de-sodio.png",
-    description: "",
+    description:
+      "O bicarbonato de sódio é um composto químico versátil com muitas aplicações na culinária e produtos de limpeza. É um agente fermentador usado em receitas de pães e bolos, e um ingrediente comum em produtos de limpeza devido às suas propriedades de neutralização de odores e capacidade de limpeza profunda. Disponível em nossa loja em alta qualidade.",
   },
   {
     name: "Boldo do Chile",
     price: 5.5,
     picture: "/assets/products/boldo-do-chile.png",
-    description: "",
+    description:
+      "O Boldo do Chile é uma planta originária da América do Sul e é conhecida por suas propriedades medicinais. O chá de Boldo é popularmente usado para aliviar problemas digestivos, como azia, gases e indigestão, além de auxiliar na desintoxicação do fígado. A planta também é usada em produtos de beleza e cuidados pessoais, como xampus e cremes para cabelos oleosos. Adquira agora mesmo o Boldo do Chile de alta qualidade em nossa loja!",
   },
   {
     name: "Capim Santo",
     price: 5.5,
     picture: "/assets/products/capim-santo.png",
-    description: "",
+    description:
+      "O Capim Santo é uma planta herbácea comumente encontrada na América Latina e é conhecida por suas propriedades medicinais e culinárias. Suas folhas possuem um aroma cítrico e são utilizadas como tempero em diversas receitas, como chás, sucos, sopas e pratos de frango e peixe. Além disso, o Capim Santo é valorizado por suas propriedades medicinais, sendo utilizado para aliviar sintomas de ansiedade, estresse e problemas digestivos.",
   },
   {
     name: "Cebola Desidratada",
     price: 5.5,
     picture: "/assets/products/cebola-desidratada.png",
-    description: "",
+    description:
+      "A Cebola Desidratada é um ingrediente culinário versátil e conveniente, que oferece o sabor e aroma característicos da cebola em diversas preparações culinárias. Ela é obtida a partir do processo de desidratação da cebola fresca, e pode ser utilizada em sopas, molhos, saladas, guisados e outros pratos que exigem o sabor da cebola. Além disso, a cebola é rica em nutrientes e compostos antioxidantes, que são benéficos para a saúde.",
   },
   {
-    name: "Cha Verde",
+    name: "Chá Verde",
     price: 5.5,
     picture: "/assets/products/cha-verde.png",
-    description: "",
+    description:
+      "O Chá Verde é uma bebida popular em todo o mundo, conhecida por seus benefícios à saúde. Ele é feito a partir das folhas da planta Camellia sinensis, que é rica em antioxidantes e outros compostos benéficos para o organismo. O Chá Verde pode ajudar na redução do risco de doenças cardiovasculares, fortalecimento do sistema imunológico, melhora da função cerebral, redução do estresse e ansiedade.",
   },
   {
     name: "Chimichurri",
     price: 5.5,
     picture: "/assets/products/chimichurri.png",
-    description: "",
+    description:
+      "O Chimichurri é um molho de tempero originário da Argentina e Uruguai, usado para realçar o sabor de carnes grelhadas, churrasco e outros pratos. É feito com ervas frescas, como salsa, orégano e manjericão, alho, vinagre e azeite de oliva. O Chimichurri é um condimento versátil e saboroso, que pode ser usado como marinada ou para adicionar sabor a qualquer prato.",
   },
   {
     name: "Colorau",
     price: 5.5,
     picture: "/assets/products/colorau.png",
-    description: "",
+    description:
+      "O colorau é um tempero em pó bastante utilizado na culinária brasileira, especialmente em pratos como o arroz, o feijão e a carne. Com sua cor vibrante e sabor característico, ele é capaz de realçar o sabor de diversos alimentos, conferindo um toque especial às suas receitas.",
   },
   {
     name: "Curry",
     price: 5.5,
     picture: "/assets/products/curry.png",
-    description: "",
+    description:
+      "O curry é um blend de temperos originário da culinária indiana, que combina diversas especiarias como cúrcuma, cominho, coentro, gengibre e pimenta, entre outras. Ele é usado para temperar pratos de carne, legumes e arroz, e seu sabor pode variar de suave a extremamente picante.",
   },
   {
     name: "Dry Rub",
     price: 5.5,
     picture: "/assets/products/dry-rub.png",
-    description: "",
+    description:
+      "Dry Rub é uma mistura de especiarias e temperos secos que são esfregados na carne antes de serem grelhados, assados ou defumados. Ele é utilizado para dar sabor e textura à carne, criando uma crosta saborosa e caramelizada na superfície. Em nossa loja, oferecemos diferentes tipos de Dry Rub, cada um com uma combinação única de especiarias e temperos, que podem variar desde sabores mais suaves até mais picantes.",
   },
   {
     name: "Feijão Supremo",
     price: 5.5,
     picture: "/assets/products/feijao-supremo.png",
-    description: "",
+    description:
+      "Este tempero específico para feijão é uma mistura de especiarias e ingredientes que são frequentemente usados na culinária brasileira para dar sabor aos feijões. Ele contém cebola e alho para fornecer um sabor salgado e aromático, além de louro em pó para adicionar uma nota de sabor terroso. O caldo de bacon e o caldo de costela são adicionados para dar um sabor rico e defumado, enquanto a páprica defumada dá um sabor adicional de fumaça e um pouco de calor.",
   },
   {
     name: "Lemon Ervas",
     price: 5.5,
     picture: "/assets/products/lemon-ervas.png",
-    description: "",
+    description:
+      "Lemon Ervas é um tempero em pó que combina o sabor cítrico do limão com um mix de ervas aromáticas, como alecrim, tomilho e manjericão. Ele é ideal para temperar carnes, peixes, saladas e legumes, conferindo um sabor fresco e delicado aos pratos. O Lemon Ervas é produzido com ingredientes de alta qualidade e cuidadosamente selecionados, garantindo um sabor autêntico e marcante em suas receitas. ",
   },
   {
     name: "Lemon Pepper",
     price: 5.5,
     picture: "/assets/products/lemon-pepper.png",
-    description: "",
+    description:
+      "Lemon Pepper é uma mistura de especiarias e temperos que combina o sabor cítrico do limão com a picância da pimenta preta. Ele é utilizado para temperar carnes, aves, peixes e saladas, conferindo um sabor marcante e um aroma fresco aos pratos. O Lemon Pepper é produzido com ingredientes de alta qualidade e cuidadosamente selecionados, garantindo um sabor autêntico e marcante em suas receitas.",
   },
   {
     name: "Oregano",
     price: 5.5,
     picture: "/assets/products/oregano.png",
-    description: "",
+    description:
+      "O orégano é uma erva aromática muito utilizada na culinária mediterrânea e latino-americana, especialmente em pratos com tomate, carne, frango, peixe e massas. Ele tem um sabor característico e levemente amargo, que pode variar de acordo com a região de cultivo e as condições climáticas. O orégano é uma fonte rica de antioxidantes e possui propriedades anti-inflamatórias, podendo trazer benefícios para a saúde.",
   },
   {
     name: "Paprica Defumada",
     price: 5.5,
     picture: "/assets/products/paprica-defumada.png",
-    description: "",
+    description:
+      "A páprica defumada é um tempero em pó feito a partir de pimentões secos que foram defumados em fogo de lenha antes de serem moídos. Ele é muito utilizado na culinária espanhola e húngara para temperar carnes, aves, peixes, legumes e arroz, conferindo um sabor defumado e levemente adocicado aos pratos. A páprica defumada é uma excelente fonte de vitamina C e antioxidantes, e pode trazer benefícios para a saúde.",
   },
   {
     name: "Pimenta do Reino",
     price: 5.5,
     picture: "/assets/products/pimenta-do-reino.png",
-    description: "",
+    description:
+      "A pimenta do reino é uma especiaria muito utilizada na culinária de todo o mundo, especialmente para temperar carnes, aves, peixes, molhos e sopas. É produzida a partir dos frutos da planta Piper nigrum, que são colhidos quando ainda verdes e secos ao sol até que fiquem pretos e enrugados. Possui um sabor picante e aromático, e é uma excelente fonte de antioxidantes e nutrientes como manganês e vitamina K.",
   },
   {
     name: "Sal Rosa do Himaláia",
     price: 5.5,
     picture: "/assets/products/sal-rosa-do-himalaia.png",
-    description: "",
+    description:
+      "O Sal Rosa do Himalaia é um tipo de sal que é extraído de depósitos antigos nas montanhas do Himalaia, na Ásia. Ele é conhecido por sua cor rosa suave e sua textura crocante, além de seu sabor distinto e levemente adocicado. O Sal Rosa do Himalaia é uma excelente fonte de minerais e oligoelementos, incluindo cálcio, magnésio e potássio, e é considerado uma opção mais saudável do que o sal de mesa convencional. ",
   },
   {
     name: "Tempero Baiano",
     price: 5.5,
     picture: "/assets/products/tempero-baiano.png",
-    description: "",
+    description:
+      "O Tempero Baiano é uma mistura de especiarias típica da culinária baiana, no Nordeste do Brasil. Ele é composto por uma variedade de ingredientes como pimenta do reino, coentro, cominho, páprica doce, açafrão-da-terra, entre outros, e é utilizado para temperar frutos do mar, peixes, carnes, aves e legumes. O Tempero Baiano confere aos pratos um sabor marcante, picante e aromático, característico da culinária baiana.",
   },
 ];
