4c46a225c9d08395355e83e0e6707e230562e139
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-22 23:41:48 -0300

    feat: add search and motion to products page

--- src/components/product-section/ProductSection.tsx
+++ src/components/product-section/ProductSection.tsx
@@ -3,35 +3,71 @@ import ProductCard from "../product-card/ProductCard";
 import "./ProductSection.scss";
 import { products } from "../../utils/products";
 import { TbSearch } from "react-icons/tb";
+import { motion } from "framer-motion";
 
 const ProductSection = () => {
   const [productsPerPage, setProductsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(0);
 
+  const [searchInput, setSearch] = useState("");
+  const lowerSearch = searchInput.toLowerCase();
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(lowerSearch)
+  );
+
   const pages = Math.ceil(products.length / productsPerPage);
   const startIndex = currentPage * productsPerPage;
   const endIndex = startIndex + productsPerPage;
-  const currentProducts = products.slice(startIndex, endIndex);
+  const currentProducts = filteredProducts.slice(startIndex, endIndex);
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 0.4,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
 
   return (
     <section className="products-section">
       <h1>Temperos e Chás</h1>
       <div className="search-box">
-        <input type="text" placeholder="Ex: Pimenta do reino" />
+        <input
+          type="text"
+          value={searchInput}
+          placeholder="Ex: Pimenta do reino"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
         <button className="search-button">
           <TbSearch />
         </button>
       </div>
-      <div className="products-wrapper">
+      <motion.div className="products-wrapper" variants={container}
+        initial="hidden"
+        animate="visible">
         {currentProducts.map((product, index) => {
           const name = product.name;
           const description = product.description;
           const price = product.price;
           const picture = product.picture;
           const object = { name, description, price, picture };
-          return <ProductCard key={index} product={object} />;
+          return <motion.div variants={item} key={index}><ProductCard product={object} /></motion.div>;
         })}
-      </div>
+      </motion.div>
       <div className="pagination-container">
         {Array.from(Array(pages), (item, index) => {
           return (
@@ -40,7 +76,9 @@ const ProductSection = () => {
               key={index}
               onClick={() => setCurrentPage(index)}
             >
-              {index + 1}
+              <span className={currentPage === index ? "active" : ""}>
+                {index + 1}
+              </span>
             </button>
           );
         })}
