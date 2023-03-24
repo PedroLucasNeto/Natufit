eceb89b408d0db5794c9c10084e717cb476ee023
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-23 21:18:04 -0300

    feat: read-more button added to product card

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Product } from "../../types/Product";
 import "./ProductCard.scss";
 
@@ -7,13 +8,19 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   const { name, price, description, picture } = product;
+  const [showMore, setShowMore] = useState(false);
 
   return (
     <div className="product-card">
       <h2>{name}</h2>
       <div className="details-div">
         <h3>Detalhes:</h3>
-        <p>{description}</p>
+        <div className="product-description">
+          <p>{showMore ? description.substring(0, description.length) : description.substring(0, 115) + '...'}</p>
+          <button onClick={() => {
+            setShowMore(!showMore)
+          }}>{showMore ? 'Ler menos...' : 'Ler mais...'}</button>
+        </div>
       </div>
       <div className="price-div">
         <h3>Preço:</h3>
