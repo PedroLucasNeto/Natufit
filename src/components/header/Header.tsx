9fdc8fbb8da39a18b2366c4b49db2faac33becc6
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-23 23:25:24 -0300

    feat: first version of mobile nav bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import MobileNav from "../mobile-nav/MobileNav";
+import ToggleButton from "../toggle-button/ToggleButton";
 import "./Header.scss";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className="header-container">
+      <div className="header-container" >
         <NavLink to="/" end>
           <img src="/assets/logo.png" alt="" />
         </NavLink>
+        <MobileNav isOpened={isOpen} openCloseNav={setIsOpen} />
+        <ToggleButton isActive={isOpen} onToggle={setIsOpen} />
         <ul className="header-nav">
           <li>
             <NavLink to="/" end>
---
+++ src/components/mobile-nav/MobileNav.tsx
@@ -0,0 +1,32 @@
+
+import { NavLink } from "react-router-dom"
+import "./MobileNav.scss"
+
+interface MobileNavProps {
+  isOpened: boolean;
+  openCloseNav: (isOpened: boolean) => void;
+}
+
+const MobileNav = ({ isOpened, openCloseNav }: MobileNavProps) => {
+  return (
+    <>
+      <div className={`mobile-nav ${isOpened ? "opened" : ""}`}>
+        <ul className="nav-list">
+          <li>
+            <NavLink to="/" onClick={() => openCloseNav(!isOpened)} end>
+              Início
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" onClick={() => openCloseNav(!isOpened)} >Produtos</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={() => openCloseNav(!isOpened)} >Contato</NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default MobileNav
