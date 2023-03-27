af8f7e1faae77a3e2279c85263438257b21fc038
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-26 22:32:02 -0300

    feat: doodle to products page and adjusts on pagination and product card

--- src/components/product-section/ProductSection.tsx
+++ src/components/product-section/ProductSection.tsx
@@ -44,7 +44,7 @@ const ProductSection = () => {
   };
 
   return (
-    <section className="products-section">
+    <>
       <div className="banner-container">
         <h1>Digite sua busca</h1>
         <div className="search-box">
@@ -62,41 +62,48 @@ const ProductSection = () => {
           </button>
         </div>
       </div>
-      <motion.div
-        className="products-wrapper"
-        variants={container}
-        initial="hidden"
-        animate="visible">
-        {currentProducts.length === 0 ? (
-          <div className="no-products">
-            <TbZoomExclamation />
-            <h2>
-              Desculpe não foi possível encontrar temperos com esse nome no nosso banco de
-              dados.
-              <br /> Por favor confira a busca e tente novamente!
-            </h2>
-          </div>
-        ) : (
-          currentProducts.map((product, index) => {
-            const name = product.name;
-            const description = product.description;
-            const price = product.price;
-            const picture = product.picture;
-            const object = { name, description, price, picture };
-            return (
-              <motion.div key={index} variants={item}>
-                <ProductCard key={index} product={object} />
-              </motion.div>
-            );
-          })
-        )}
-      </motion.div>
-      <Pagination
-        pages={pages}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      />
-    </section>
+      <section className="products-section">
+        <Pagination
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+        <motion.div
+          className="products-wrapper"
+          variants={container}
+          initial="hidden"
+          animate="visible">
+          {currentProducts.length === 0 ? (
+            <div className="no-products">
+              <TbZoomExclamation />
+              <h2>
+                Desculpe não foi possível encontrar temperos com esse nome no nosso banco
+                de dados.
+                <br /> Por favor confira a busca e tente novamente!
+              </h2>
+            </div>
+          ) : (
+            currentProducts.map((product, index) => {
+              const name = product.name;
+              const description = product.description;
+              const price = product.price;
+              const picture = product.picture;
+              const object = { name, description, price, picture };
+              return (
+                <motion.div key={index} variants={item}>
+                  <ProductCard key={index} product={object} />
+                </motion.div>
+              );
+            })
+          )}
+        </motion.div>
+        <Pagination
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </section>
+    </>
   );
 };
 
