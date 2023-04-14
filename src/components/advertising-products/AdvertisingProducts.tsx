fb9c57712d61138600cbea77b50e6dfcd9e1ccf3
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-04-14 14:42:45 -0300

    feat: change to test

--- src/components/advertising-products/AdvertisingProducts.tsx
+++ src/components/advertising-products/AdvertisingProducts.tsx
@@ -3,26 +3,28 @@ import "./AdvertisingProducts.scss";
 
 const AdvertisingProducts = () => {
   return (
-    <section className="ad-section">
-      <div className="ad-titles">
-        <h1>Conheça nossos Produtos</h1>
-        <p>
-          Não perca mais tempo sem experimentar nossos temperos. Peça agora e transforme
-          suas receitas em obras de arte culinárias.
-        </p>
-      </div>
-      <Link
-        to="/products/"
-        role="button"
-        onClick={() =>
-          window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-          })
-        }>
-        Saiba Mais!
-      </Link>
-    </section>
+    <>
+      <section className="ad-section">
+        <div className="ad-titles">
+          <h1>Conheça nossos Produtos</h1>
+          <p>
+            Não perca mais tempo sem experimentar nossos temperos. Peça agora e transforme
+            suas receitas em obras de arte culinárias.
+          </p>
+        </div>
+        <Link
+          to="/products/"
+          role="button"
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            })
+          }>
+          Saiba Mais!
+        </Link>
+      </section>
+    </>
   );
 };
 
